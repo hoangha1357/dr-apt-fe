c0087205c51e2f2c9cd1d1689c799e32a1748b57
hoangha1357 <76683867+hoangha1357@users.noreply.github.com>
2022-11-19 09:12:55 +0700

    add-respo

---
+++ src/component/Page1.js
@@ -0,0 +1,12 @@
+//import {Outlet} from "react-router-dom"
+
+function HomePage({num}) {
+
+    return (
+        <div>
+            <h1>Page {num}</h1>
+        </div>
+    )
+}
+
+export default HomePage;
---
+++ src/routes.js
@@ -0,0 +1,24 @@
+import HomePage from './component/Home';
+import Page from './component/Page1';
+import NotFound from './component/NotFound';
+import Sidebar from './layouts/SideBar'
+const routes = [
+    {
+        path: "/",
+        element: <HomePage />,
+    },{
+        path: "page1",
+        element: <Sidebar/>,
+        children: [
+            {
+                path: "page2",
+                element: <Page num={2}/>,
+            },
+        ],
+    },
+    {
+        path: "*",
+        element: <NotFound/>
+    }
+]
+export default routes;
