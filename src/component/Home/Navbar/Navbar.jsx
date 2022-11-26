1042597e9c50a18bc7474a953832c6d0a4a62885
BaoKha-S <76689534+BaoKha-S@users.noreply.github.com>
2022-11-26 09:49:19 +0700

    k

--- src/component/Home/Navbar/Navbar.jsx
+++ src/component/Home/Navbar/Navbar.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { useState } from "react";
 // import Toggle from "../Toggle/Toggle";
-import 'boxicons'
+import "boxicons";
 import "./Navbar.scss";
 import "../../../sass/global.scss";
-import Noti from "./Noti/Noti.jsx"
+import Noti from "./Noti/Noti.jsx";
+import Search from "./Search/Search.jsx";
 const Navbar = () => {
-  const [buttonNoti,setButtonNoti] = useState(false)
+  const [buttonNoti, setButtonNoti] = useState(false);
+  const [buttonSearch, setButtonSearch] = useState(false);
+  function handleSearch(set) {
+    setButtonNoti(false);
+    return setButtonSearch(!set);
+  }
+  function handleNoti(set) {
+    setButtonSearch(false);
+    return setButtonNoti(!set);
+  }
   return (
     <div className="n-wrapper">
       <div className="n-left">
@@ -24,10 +34,14 @@ const Navbar = () => {
         </div>
       </div>
       <div className="n-right">
-         <box-icon name='search' ></box-icon>
-         <box-icon onClick={() => setButtonNoti(!buttonNoti)} name='bell' ></box-icon>
+        <box-icon
+          onClick={() => handleSearch(buttonSearch)}
+          name="search"
+        ></box-icon>
+        <box-icon onClick={() => handleNoti(buttonNoti)} name="bell"></box-icon>
       </div>
       <Noti trigger={buttonNoti}></Noti>
+      <Search trigger={buttonSearch}></Search>
     </div>
   );
 };
--- src/component/Home/Navbar/Search/Search.jsx
+++ src/component/Home/Navbar/Search/Search.jsx
@@ -1,19 +1,20 @@
 import React from "react";
-import "./Search.scss"
+import "./Search.scss";
+// import "./style.scss";
 
 function Search(props) {
-    return (props.trigger) ? (
-        <div class="box-search">
-            <div class="border"></div>
-            <form action="" class="search-form">
-              <input
-                type="search"
-                placeholder="Search here..."
-                id="search-box"
-              />
-              <label for="search-box" class="bx bx-search"></label>
-            </form>
-          </div>
-    ) : "";
+  return props.trigger ? (
+    <div class="box-search">
+      <div class="border"></div>
+      <form action="" class="search-form">
+        <input type="search" placeholder="Search here..." id="search-box" />
+        <label for="search-box">
+          <box-icon name="search"></box-icon>
+        </label>
+      </form>
+    </div>
+  ) : (
+    ""
+  );
 }
-export default Search
+export default Search;
